94a5eccc429566b36cac59aefb19ca5696365dac
Toqeer-Mayo <toqeerriaz318@gamail.com>
2022-09-06 19:11:04 +0500

    home page complete.

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Weather from './components/Weather'
+function Home() {
+  return (
+    <div>
+        <Weather/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
